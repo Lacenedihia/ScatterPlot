ea4e68273c448b09fbccd86cbf962477652cb44e
Lacene Dihia <152735797+Lacenedihia@users.noreply.github.com>
2024-02-14 14:25:07 +0100

    Update axes.js

--- axes.js
+++ axes.js
@@ -49,7 +49,7 @@ export const axes = (
     .attr('alignment-baseline', 'hanging')
     .attr('text-anchor', 'middle')
     .attr('font-family', 'sans-serif')
-    .attr('font-size', "7vw")
+    .attr('font-size', "8vw")
     .text(xAxisLabel);
 
   selection
@@ -68,6 +68,6 @@ export const axes = (
       'y',
       xScale.range()[0] - yAxisLabelOffset
     )
-    .attr('font-size', "3vw")
+    .attr('font-size', "8vw")
     .text(yAxisLabel);
 };
